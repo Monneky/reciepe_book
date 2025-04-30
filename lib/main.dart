4c905696c1d06a666288ac04f563dfb39270e68f
Cesar Ivann Llamas Macias <ivannos324@gmail.com>
2025-04-29 23:00:30 -0600

    Agrega configuración de VSCode y reestructura la pantalla principal del libro de recetas con un TabBar y una nueva pantalla de inicio.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curso_flutter/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -20,36 +21,20 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text('Reciepe Book', style: TextStyle(color: Colors.white)),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 125,
-                width: 125,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(),
-                ),
-              ),
-              SizedBox(width: 26),
-              Column(
-                children: <Widget>[
-                  Text('Lasagna'),
-                  Text('Ivos Llamas'),
-                  Container(height: 2, width: 75, color: Colors.orange),
-                ],
-              ),
-            ],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('Reciepe Book', style: TextStyle(color: Colors.white)),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            tabs: [Tab(icon: Icon(Icons.home), text: 'Home')],
           ),
         ),
+        body: TabBarView(children: [HomeScreen()]),
       ),
     );
   }
